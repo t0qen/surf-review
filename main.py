41615ef4aa2ed1c22d450e7763e0db7b14ff0d48
t0qen <mybeautifuladress@gmail.com>
2026-07-09 21:16:17 +0200

    analyse and hours match works !

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
 openmeteo = openmeteo_requests.Client(session = retry_session)
 
 max_score_tolerance = 0.5 # plus on augmente ce nombre, plus il y aura d'horaires pouvant convenir
+tolerance_step = 0.5 # par combien on va augmenter la tolerance a chaque probleme
 
-min_wave_height = 0.7
+min_wave_height = 0.4
 max_wave_height = 3
 min_wave_period = 6
 max_wave_period = 14
@@ -27,6 +28,12 @@ beach_direction = 45
 def map_range(x, in_min, in_max, out_min, out_max):
   return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
 
+# vient de chatgpt
+def compare_arrays(arr):
+    result = set(arr[0]) # on transforme la permiere liste en ensemble
+    for i in arr[1:]: # on loop chaque liste en entree
+        result &= set(i) # on garde que les valeurs communes avec celle d'avant
+    return list(result)
 
 def requests_forecast():
     #  (1) first request: wind direction and speed
@@ -95,7 +102,7 @@ def score_data(data, type):
     return score
      
 
-def get_dicts():
+def get_dicts(tolerance):
     # on recup les tableaux pour chaque categories avec requests_forecast(),
     # on analyse ces donnees et on fait des dict qui contiennent toutes les donnees
     # importantes de la categorie, comme le score moyen, le score max et poru quelles heures, etc
@@ -130,12 +137,12 @@ def get_dicts():
             hour += 1
 
 
-        average_score = total_score / 24
-        average_data = total_data / 24
+        average_score = float(total_score) / 24
+        average_data = float(total_data) / 24
 
         if max_score:
             # vient de chatgpt, on regarde l'index (l'heure) ou le meilleur score -1 est atteint
-            index_max_score = [i for i, val in enumerate(scores) if val > max_score - max_score_tolerance]
+            index_max_score = [i for i, val in enumerate(scores) if val > max_score - tolerance]
         else:
             index_max_score = [0]
 
@@ -146,7 +153,7 @@ def get_dicts():
             "max_score": max_score,
             "average_score": average_score,
             "average_data": average_data,
-            "best hour": index_max_score
+            "best_hours": index_max_score
         } 
 
         if current == 0:
@@ -164,16 +171,89 @@ def get_dicts():
 
 
 
-dicts = get_dicts()
-print("-------")
-for key, value in dicts[0].items():
-    print(f"{key}: {value}")
-print("-------")
-for key, value in dicts[1].items():
-    print(f"{key}: {value}")
-print("-------")
-for key, value in dicts[2].items():
-    print(f"{key}: {value}")
-print("-------")
-for key, value in dicts[3].items():
-    print(f"{key}: {value}")
+def main():
+    # fonc generale : recupere, analyse, compare, deduis, transmet
+
+    current_tolerance = max_score_tolerance
+    result = { # va etre remplie dans le code pour etre transmise a ntfy
+        "title": "",
+        "content": "",
+        "tags": ""
+    }
+    
+    
+    # 1) on analyse les donnees une premiere fois
+    dicts = get_dicts(current_tolerance)
+
+    # 2) on verif si les conditions peuvent convenir (si max score != 0)
+    dict_speed = dicts[0]
+    dict_dir = dicts[1]
+    dict_height = dicts[2]
+    dict_period = dicts[3]
+    
+    # si une des categ n'a que des 0 comme score, on arrete le programme, on ne peut pas faire de surf
+    test = dict_speed["max_score"] * dict_dir["max_score"] * dict_height["max_score"] * dict_period["max_score"]
+    print("Test: ", test)
+    if not test:
+        # une des categ est nulle, on determine laquelle avant d'envoyer la reponse
+        result["title"] = "Pas aujourd'hui pour le surf"
+        result["tags"] = "warning"
+        bad_categ = "En effet, "
+        if not dict_speed["max_score"]:
+            bad_categ += f"le vent depasse les {round(dict_speed['average_data'], 2)}km/h."
+        if not dict_dir["max_score"]:
+            bad_categ += f"en moyenne, la direction du vent est de {round(dict_dir['average_data'], 2)} degres."
+        if not dict_height["max_score"]:
+            bad_categ += f"la moyenne des vagues ne depasse pas les {round(dict_height['average_data'], 2)}m."
+        if not dict_period["max_score"]:
+            bad_categ += f"la periode de vague est de {round(dict_period['average_data'], 2)}s."
+        result["content"] = bad_categ
+        return result
+    else:
+
+        selected_hours = []
+        i = 0
+        while True:
+            best_hours = [
+                dict_speed["best_hours"],
+                dict_dir["best_hours"],
+                dict_height["best_hours"],
+                dict_period["best_hours"]
+            ]
+            selected_hours = compare_arrays(best_hours)
+            if selected_hours:
+                break
+
+            # tant qu'on na pas d'horaires on baisse la tolerance
+            current_tolerance += tolerance_step
+            dicts = get_dicts(current_tolerance)
+            dict_speed = dicts[0]
+            dict_dir = dicts[1]
+            dict_height = dicts[2]
+            dict_period = dicts[3]
+    
+
+            i += 1
+        result["title"] = "Une petite session de surf ?"
+        result["content"] = f"Avec une tolerance de {int(current_tolerance)}, les meilleurs heures sont: "
+        result["content"] += "h, ".join(map(str, selected_hours)) + "h"
+        result["content"] += ". Voici un apercu des donnees analysee, en moyenne: \n"
+        result["content"] += f"Vitesse du vent: {round(dict_speed['average_data'], 2)}km/h, score: {round(dict_speed['average_score'], 2)}."
+        result["content"] += f" Ecart du vent: {round(dict_dir['average_data'], 2)} degres, score: {round(dict_dir['average_score'], 2)}."
+        result["content"] += f" Hauteur des vagues: {round(dict_height['average_data'], 2)}m, score: {round(dict_height['average_score'], 2)}."
+        result["content"] += f" Periode des vagues: {round(dict_period['average_data'], 2)}s, score: {round(dict_period['average_score'], 2)}."
+        return result
+
+print(main())
+# print("-------")
+# for key, value in dicts[0].items():
+#     print(f"{key}: {value}")
+# print("-------")
+# for key, value in dicts[1].items():
+#     print(f"{key}: {value}")
+# print("-------")
+# for key, value in dicts[2].items():
+#     print(f"{key}: {value}")
+# print("-------")
+# for key, value in dicts[3].items():
+#     print(f"{key}: {value}")
